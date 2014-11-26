2d9ffd3fe85583a7dedd5fd8a8c0685deacb90af
BhavnaMakkar <bhavnamakkar01@gmail.com>
2014-11-26 12:26:10 +0530

    Game Score Information System
    History and Activity of Players

---
+++ check.php
@@ -0,0 +1,14 @@
+<?php
+
+$username='root';
+$password='';
+$dbname ='zootar';
+
+// Create connection
+$conn = new mysqli( 'localhost',$username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+?>
